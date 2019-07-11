9e95ac25a964d9d0a0b999f4dd4933dfee8a6de4
--gravekeeper1 <bhoang12345@gmail.com>
2019-07-11 12:26:32 -0500

    Main.cpp updated

--- Connect_Four/Main.cpp
+++ Connect_Four/Main.cpp
@@ -10,38 +10,47 @@
 
 using namespace std;
 
-Board b;
+Board b(6, 7);
 void showgameboard(vector<vector<string>> board);
+string switchplayer(string currentplayer);
 int main() {
-	
-	string p_1 = "R";
-	string p_2 = "Y";
+
+	string player = "R";
+	int col;
 	string user1_input;
 	string user2_input;
 	b.FillBoard();
 	
 	vector<vector<string>> board_output = b.getboard();
+	
 	// Place moves between 2 players 
-	b.place_piece(p_2, board_output, 0);
 
-	b.place_piece(p_2, board_output, 3);
-	
-	
 	// call check for winner
-	b.check_for_winner(b.get_lastmovex(), b.get_lastmovey(), p_2, board_output);
+	//b.check_for_winner(b.get_lastmovex(), b.get_lastmovey(), p_2, board_output);
 	//b.check_for_winner(b.get_lastmovex(), b.get_lastmovey(), p_1, board_output);
+
 	while (true) {
 		showgameboard(board_output);
-		
+		std::cout << "Player " + player + ", enter the column where you'd like to drop your piece: " << endl;
+		cin >> col;
+		if (b.place_piece(player, board_output, col)) {
+			if (b.check_for_winner(b.get_lastmovex(), b.get_lastmovey(), player, board_output)) {
+				cout << "player " + player + " Wins!" << endl;
+				//showgameboard(board_output);
+				break;
+			}
+			
+			player = switchplayer(player);
+		}
 	}
-	
+
 	system("pause");
 	return 0;
 }
 
 void showgameboard(vector<vector<string>> board) {
-	for (int i = 0; i < 6; i++) {
-		for (int j = 0; j < 7; j++) {
+	for (int i = 0; i < b.getboard().size(); i++) {
+		for (int j = 0; j < b.getboard()[i].size(); j++) {
 			cout << board[i][j] << " ";
 		}
 		cout << endl;
